70dc724d86af8443787cfcc63ce86e47e30b9635
Henning Leutz <leutz@pcsg.de>
2015-08-10 10:41:15 +0200

    buttons

---
+++ src/QUI/Buttons/Events.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file contains \QUI\Buttons\Events
+ */
+namespace QUI\Buttons;
+
+/**
+ * Event handling
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+class Events
+{
+    static function onTemplateGetHeader($Template)
+    {
+        $Template->extendHeaderWithCSSFile(URL_OPT_DIR .'quiqqer/buttons/bin/buttons.css');
+    }
+}
