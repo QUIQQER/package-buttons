fb771ee6bde0e858b9e32afdd330ac3a3ee02b4e
Jan Wennrich <jan.wennrich@pcsg.de>
2018-11-20 18:24:32 +0100

    refactor: added parameter type & docs

--- src/QUI/Buttons/Events.php
+++ src/QUI/Buttons/Events.php
@@ -3,6 +3,7 @@
 /**
  * This file contains \QUI\Buttons\Events
  */
+
 namespace QUI\Buttons;
 
 /**
@@ -12,8 +13,14 @@ namespace QUI\Buttons;
  */
 class Events
 {
-    public static function onTemplateGetHeader($Template)
+    /**
+     * Called when a template header is loaded.
+     * Extends the header with the CSS button styles.
+     *
+     * @param \QUI\Template $Template
+     */
+    public static function onTemplateGetHeader(\QUI\Template $Template)
     {
-        $Template->extendHeaderWithCSSFile(URL_OPT_DIR .'quiqqer/buttons/bin/buttons.css');
+        $Template->extendHeaderWithCSSFile(URL_OPT_DIR . 'quiqqer/buttons/bin/buttons.css');
     }
 }
